bfe9242750f7f61ba9e7a3e471079418a353dc32
mykdmrk <118302400+mykdmrk@users.noreply.github.com>
2023-02-08 08:53:57 +0530

    Update Banner.js

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -35,7 +35,7 @@ export default function Banner() {
               </button>
             </a>
             <a
-              href="https://discord.gg/Vcy7Q55f"
+              href="https://discord.gg/BgRyZsW9"
               target="_blank"
               rel="noreferrer"
               className="w-[22%]"
